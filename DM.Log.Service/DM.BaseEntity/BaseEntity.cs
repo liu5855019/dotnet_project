c78576d4dcc09af672e8378d87aee9e0b753655e
daimu <258141764@qq.com>
2022-11-20 16:21:14 +0800

    add entity project

---
+++ DM.Log.Service/DM.BaseEntity/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace DM.BaseEntity
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class BaseEntity
+    {
+        public long Id { get; set; }
+
+        [StringLength(50)]
+        public string CreateBy { get; set; }
+
+        public DateTime? CreateDt { get; set; }
+
+        [StringLength(50)]
+        public string UpdateBy { get; set; }
+
+        public DateTime? UpdateDt { get; set; }
+
+    }
+}
+
