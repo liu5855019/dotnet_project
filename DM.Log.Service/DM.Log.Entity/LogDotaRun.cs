16365594166bf37ea9d608323d8e197171d497c3
daimu <jinlin@wangdou-hbean.com>
2022-12-27 18:03:17 +0800

    Update LogDotaRun.cs

--- DM.Log.Service/DM.Log.Entity/LogDotaRun.cs
+++ DM.Log.Service/DM.Log.Entity/LogDotaRun.cs
@@ -4,7 +4,11 @@
 
     public class LogDotaRun : BaseEntity
     {
-        
+        public long DeviceId { get; set; }
+
+        public long GroupId { get; set; }
+
+        public bool IsShop { get; set; }
     }
 }
 
