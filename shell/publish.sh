29af3b8c85fa0b321a09a00a565c4a87ec2c5507
daimu <jinlin@wangdou-hbean.com>
2023-01-04 18:02:11 +0800

    update

--- shell/publish.sh
+++ shell/publish.sh
@@ -15,3 +15,37 @@ dotnet publish \
     -c Release \
     -o /root/dockers/projects/log_service/publish
 
+
+
+
+
+
+
+
+
+#! /bin/bash
+
+cd /root/shell/incident/CAD-AppServer-IncidentService
+pwd
+
+# git checkout -b cad_team_a remotes/origin/cad_team_a
+git checkout incident_publish
+git pull
+
+cd /root/shell/incident/CAD-AppServer-IncidentService/SourceCode/Server/Stee.FGMS.CAD.IncidentSrv.GrpcService/Stee.FGMS.CAD.IncidentSrv.GrpcService
+rm -rf ./bin/publish
+dotnet publish --force "Stee.FGMS.CAD.IncidentSrv.GrpcService.csproj" -c Release -o ./bin/publish
+echo "publish ok"
+cp -r ./bin/publish /root/devops/cad_incident/
+echo "cp ok"
+
+cd /root/devops
+docker-compose up -d --build cad_incident
+
+# 放开下面两行 即可自动更新 cad.pb, 重启 envoy9443
+cd /root/shell
+bash update_cad_pb.sh
+
+docker logs -f cad_incident
+
+
