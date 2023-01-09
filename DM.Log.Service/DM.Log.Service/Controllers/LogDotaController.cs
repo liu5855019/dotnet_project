b16b14e2784dc5250d271a87c258494b2be1c079
daimu <jinlin@wangdou-hbean.com>
2023-01-09 18:05:30 +0800

    add LogDotaController

---
+++ DM.Log.Service/DM.Log.Service/Controllers/LogDotaController.cs
@@ -0,0 +1,62 @@
+﻿
+namespace DM.Log.Service.Controllers
+{
+    using DM.Log.Entity;
+    using Microsoft.AspNetCore.Mvc;
+    using NLog;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class LogDotaController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger logger = LogManager.GetCurrentClassLogger();
+
+        public LogDotaController()
+        {
+        }
+
+        [HttpGet]
+        public List<string> AddLog(long DeviceId, long GroupId, bool IsShop)
+        {
+                    
+        //LogDotaRun
+
+            return new List<string> {  };
+        }
+
+        [HttpGet(Name = "GetWeatherForecast4")]
+        public IEnumerable<WeatherForecast> Get2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost(Name = "GetWeatherForecast5")]
+        public IEnumerable<WeatherForecast> Get3()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
+
+
+
