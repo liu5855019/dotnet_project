dc058418aa50c3e82010d99e652d85c39ba462ba
daimu <jinlin@wangdou-hbean.com>
2023-01-16 17:54:15 +0800

    Update publish.sh

--- shell/publish.sh
+++ shell/publish.sh
@@ -3,12 +3,14 @@
 echo "start publish..."
 pwd
 
+echo "start git pull"
 git pull
 
 
 cd ../DM.Log.Service/DM.Log.Service
 pwd
 rm -rf ./bin/publish
+echo "start dotnet publish"
 dotnet publish --force "DM.Log.Service.csproj" -c Release -o ./bin/publish
 echo "publish ok"
 cp -rf ./bin/publish/* /root/dockers/dm_log/publish/
